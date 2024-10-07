6c062e8e0ffad0b3ee3e3fc3bed2828e0bcb1d03
giwon.seo <giwon.seo@neolab.net>
2024-10-07 11:30:43 +0900

    Initial Commit

---
+++ PokoroWebApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PokoroWebApp
+//
+//  Copyright (c) 2024 NeoLAB Convergence. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ PokoroWebAppTests/PokoroWebAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  PokoroWebAppTests.swift
+//  PokoroWebAppTests
+//
+//  Copyright (c) 2024 NeoLAB Convergence. All rights reserved.
+//
+
+import Testing
+@testable import PokoroWebApp
+
+struct PokoroWebAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
